59ad2eb0af54afddd2c44109b5012a250f46ce2f
Lars Möller <lars@Larss-MBP.fritz.box>
2023-01-13 12:17:20 +0100

    Added first project structure using cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Example project - End to end - Churn prediction',
+    author='lars_foehr',
+    license='MIT',
+)
